f5b6950a2ac6e5165dbadc81f14b556c7001cc61
Srinivasulu D <56268088+srinivasulu0514@users.noreply.github.com>
2026-01-26 10:35:53 +0530

    Initialize Flask app with database connection

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import pymysql
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+
+@app.route("/data")
+def data():
+    conn = pymysql.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT 'Hello from RDS 3-Tier DB!'")
+    result = cursor.fetchone()
+
+    return jsonify({"message": result[0]})
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
